344bbc8bd1945b319b60dc88f821467b4e30e761
Guido de Giovanni <guidito.dg86@gmail.com>
2022-01-13 00:55:36 -0300

    Service working

--- flask/microservice-api.py
+++ flask/microservice-api.py
@@ -1,16 +1,37 @@
-from json.decoder import JSONDecodeError
 from flask import Flask
 from flask_restful import request ,Resource, Api
-import requests
+from datetime import datetime
 
 app = Flask(__name__)
 api = Api(app)
 
-class TimestampService(Resource):
+class TimestampServiceNull(Resource):
     def get(self):
-        return {"message": "test"}
+        parsed_date = datetime.utcnow()
+        return { "utc" : datetime.strftime(parsed_date, "%a, %d %b %Y %H:%M:%S GMT" ),
+                "unix": int((datetime.timestamp(parsed_date) - datetime.timestamp(datetime.strptime("1970-01-01", "%Y-%m-%d")))*1000)
+                }
 
-api.add_resource(TimestampService, '/')
+class TimestampService(Resource):
+    def get(self, received_date):
+        try:
+            parsed_date = datetime.strptime(received_date, "%Y-%m-%d")
+            return { "utc" : datetime.strftime(parsed_date, "%a, %d %b %Y %H:%M:%S GMT" ),
+                     "unix": int((datetime.timestamp(parsed_date) - datetime.timestamp(datetime.strptime("1970-01-01", "%Y-%m-%d")))*1000)
+                    }
+        except:
+            try:
+                parsed_unix = int(received_date)
+                date_parsed =  datetime.utcfromtimestamp(parsed_unix/1000)
+                return { "utc" : datetime.strftime(date_parsed, "%a, %d %b %Y %H:%M:%S GMT" ),
+                     "unix": parsed_unix
+                    }
+            except:
+                return {"error" : "Invalid Date" }
+            
+        
+api.add_resource(TimestampServiceNull, '/api/')
+api.add_resource(TimestampService, '/api/<string:received_date>')
 
 if __name__ == '__main__':
     app.run(debug=True)
